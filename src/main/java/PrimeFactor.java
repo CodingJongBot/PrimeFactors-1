22e9ea710ff94dbc31109fe4573c0d0858df1712
jongwon <oasis9311@gmail.com>
2025-06-04 10:16:27 +0900

    [feature] 32p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -3,8 +3,11 @@ import java.util.List;
 import java.util.Objects;
 
 public class PrimeFactor {
-    public List<Integer> of(int i) {
+    public List<Integer> of(int number) {
         List<Integer> facotrs = new ArrayList<>();
+        if (number==2){
+            facotrs.add(2);
+        }
         return facotrs;
     }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
@@ -6,14 +7,25 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
+    PrimeFactor primeFactor;
+
+    @BeforeEach
+    void setUp() {
+        primeFactor = new PrimeFactor();
+    }
+
     @Test
     void name() {
-        assertEquals(1,1);
+        assertEquals(1, 1);
     }
 
     @Test
     void testPrimeFactorOf1() {
-        PrimeFactor primeFactor = new PrimeFactor() ;
-        assertEquals(Arrays.asList(),primeFactor.of(1));
+        assertEquals(Arrays.asList(), primeFactor.of(1));
+    }
+
+    @Test
+    void testPrimeFacotrOf2() {
+        assertEquals(Arrays.asList(2), primeFactor.of(2));
     }
 }
