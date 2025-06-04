5b1a0c920e9d6b284d9d3dd895dfb198450fbdde
jongwon <oasis9311@gmail.com>
2025-06-04 10:12:12 +0900

    [feature] 24p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class PrimeFactor {
+    public List<Integer> of(int i) {
+        List<Integer> facotrs = new ArrayList<>();
+        return facotrs;
+    }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,10 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
     @Test
     void name() {
         assertEquals(1,1);
+    }
+
+    @Test
+    void testPrimeFactorOf1() {
+        PrimeFactor primeFactor = new PrimeFactor() ;
+        assertEquals(Arrays.asList(),primeFactor.of(1));
     }
 }
